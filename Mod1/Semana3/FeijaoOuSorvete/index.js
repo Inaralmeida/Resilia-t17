493c079d8b4e16fdf5dffb817eca8040c7854243
inaralmeida <inara.ralmeida@gmail.com>
2022-04-06 11:17:15 -0300

    Adicionado conteudo das aulas de 1 a 10

---
+++ Mod1/Semana3/FeijaoOuSorvete/index.js
@@ -0,0 +1,24 @@
+var nome = "Josiane";
+var idade = 23;
+console.log(nome);
+console.log(idade);
+
+nome = "Patrick";
+idade = 34;
+console.log("variavel nome " + typeof nome);
+console.log("variavel idade " + typeof idade);
+
+var primeiroNumero = 75;
+var segundoNumero = 34;
+console.log(primeiroNumero + segundoNumero);
+console.log(primeiroNumero - segundoNumero);
+console.log(primeiroNumero * segundoNumero);
+console.log(primeiroNumero / segundoNumero);
+
+console.log("nome " + nome + "\n idade " + idade);
+
+primeiroNumero = primeiroNumero + segundoNumero;
+console.log(primeiroNumero);
+
+//tipo de ...
+console.log(typeof primeiroNumero);
---
+++ Mod1/Semana3/computando/index.js
@@ -0,0 +1,4 @@
+console.log("Salve T17");
+console.log("To funcionando");
+console.log("usei o log");
+console.log("Usei o clg");
